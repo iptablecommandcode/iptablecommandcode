bde9125d27a981661a1788df8052c3d42b0e0a7f
iptablecommandcode <iptablecommandcode@gmail.com>
2022-03-01 17:28:18 +0900

    DB접속 설정 pom 변경

--- src/test/java/me/synology/freash97/iptablecommandcode/IptablecommandcodeApplicationTests.java
+++ src/test/java/me/synology/freash97/iptablecommandcode/IptablecommandcodeApplicationTests.java
@@ -1,6 +1,5 @@
 package me.synology.freash97.iptablecommandcode;
 
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
