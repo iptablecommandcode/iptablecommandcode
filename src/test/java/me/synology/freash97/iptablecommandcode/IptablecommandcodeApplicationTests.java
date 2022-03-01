86df73542e4e45f2ae87f7241e2c9f52e7ab2dce
iptablecommandcode <iptablecommandcode@gmail.com>
2022-03-01 16:35:00 +0900

    DB접속 암호화

--- src/test/java/me/synology/freash97/iptablecommandcode/IptablecommandcodeApplicationTests.java
+++ src/test/java/me/synology/freash97/iptablecommandcode/IptablecommandcodeApplicationTests.java
@@ -1,5 +1,6 @@
 package me.synology.freash97.iptablecommandcode;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +11,23 @@ class IptablecommandcodeApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void jasypt() {
+        String url = "";
+        String username = "";
+        String password = "";
+
+        System.out.println(jasyptEncoding(url));
+        System.out.println(jasyptEncoding(username));
+        System.out.println(jasyptEncoding(password));
+    }
+
+    public String jasyptEncoding(String value) {
+
+        String key = "my_jasypt_key";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
+    }
 }
