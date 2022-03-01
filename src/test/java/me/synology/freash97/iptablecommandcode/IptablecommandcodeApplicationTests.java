8fc2a9ba65aa6f6ac9dd840fa610a09e344abccb
iptablecommandcode <iptablecommandcode@gmail.com>
2022-03-01 16:47:27 +0900

    DB접속 설정

--- src/test/java/me/synology/freash97/iptablecommandcode/IptablecommandcodeApplicationTests.java
+++ src/test/java/me/synology/freash97/iptablecommandcode/IptablecommandcodeApplicationTests.java
@@ -10,24 +10,4 @@ class IptablecommandcodeApplicationTests {
     @Test
     void contextLoads() {
     }
-
-    @Test
-    void jasypt() {
-        String url = "";
-        String username = "";
-        String password = "";
-
-        System.out.println(jasyptEncoding(url));
-        System.out.println(jasyptEncoding(username));
-        System.out.println(jasyptEncoding(password));
-    }
-
-    public String jasyptEncoding(String value) {
-
-        String key = "my_jasypt_key";
-        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
-        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
-        pbeEnc.setPassword(key);
-        return pbeEnc.encrypt(value);
-    }
 }
