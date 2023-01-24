37028e585b3e4ed39f1781f01b3668bd793def0e
iptablecommandcode <iptablecommandcode@gmail.com>
2023-01-24 14:36:20 +0900

    First Start

---
+++ src/main/java/me/synology/freash97/IptablecommandcodeApplication.java
@@ -0,0 +1,13 @@
+package me.synology.freash97;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IptablecommandcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IptablecommandcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/synology/freash97/IptablecommandcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.synology.freash97;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IptablecommandcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
