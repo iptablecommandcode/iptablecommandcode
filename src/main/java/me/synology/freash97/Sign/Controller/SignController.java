6318144e7eb6f259fe8c147432155b615de83d50
iptablecommandcode <iptablecommandcode@gmail.com>
2023-02-02 00:36:41 +0900

    화면 소스 static 전환 및 기본 접속 가능한 로직 수정

--- src/main/java/me/synology/freash97/Sign/Controller/SignController.java
+++ src/main/java/me/synology/freash97/Sign/Controller/SignController.java
@@ -1,26 +1,22 @@
 package me.synology.freash97.Sign.Controller;
 
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import me.synology.freash97.Sign.Entity.UserEntity;
 import me.synology.freash97.Sign.Service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 
-
-@Controller
 @Slf4j
+@Controller
+@NoArgsConstructor
 public class SignController {
     private final String value = "Sign/";
 
-    private  final UserService userService;
+    private UserService userService;
 
-    SignController(UserService userService){
-        this.userService = userService;
-    }
     @GetMapping(value + "Sign_In")
     public ModelAndView Sign_In(){
         ModelAndView modelAndView = new ModelAndView();
--- src/main/java/me/synology/freash97/index/Conroller/indexController.java
+++ src/main/java/me/synology/freash97/index/Conroller/indexController.java
@@ -1,11 +1,15 @@
 package me.synology.freash97.index.Conroller;
 
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class indexController {
+@Slf4j
+@NoArgsConstructor
+class indexController {
     @GetMapping("index")
     public ModelAndView index(){
         ModelAndView modelAndView = new ModelAndView();
