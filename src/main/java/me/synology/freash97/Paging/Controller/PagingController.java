6c48df925e3badb9508b0e0f3dc8ac806d925c78
kimjoochang <kimjc0707@naver.com>
2023-02-06 23:45:02 +0900

    페이징 관련 추가

---
+++ src/main/java/me/synology/freash97/Paging/Controller/PagingController.java
@@ -0,0 +1,17 @@
+package me.synology.freash97.Paging.Controller;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+@NoArgsConstructor
+public class PagingController {
+
+//    @GetMapping("paging/")
+//    public long noticeList() {
+//        return;
+//    }
+}
