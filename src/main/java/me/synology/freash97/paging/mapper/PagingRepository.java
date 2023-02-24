c68938bffb30a6c625b07b2028b8e023cb6b6b69
kimjoochang <kimjc0707@naver.com>
2023-02-24 19:38:03 +0900

    paging.xml resultMap 경로 수정

--- src/main/java/me/synology/freash97/paging/mapper/PagingRepository.java
+++ src/main/java/me/synology/freash97/paging/mapper/PagingRepository.java
@@ -1,8 +1,11 @@
 package me.synology.freash97.paging.mapper;
 
+import me.synology.freash97.notice.entity.NoticeEntity;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface PagingRepository {
-    int getToTalCount();
+    List<NoticeEntity> findAllNotice();
 }
