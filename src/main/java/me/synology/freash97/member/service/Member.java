540c542fb01d112262d4d8effa77e97f4b5a9602
iptablecommandcode <iptablecommandcode@gmail.com>
2023-02-27 23:57:17 +0900

    Member 값 오류시 오류 로직 확인

--- src/main/java/me/synology/freash97/member/service/Member.java
+++ src/main/java/me/synology/freash97/member/service/Member.java
@@ -20,7 +20,7 @@ public class Member {
         log.debug("objToEntity에서 Object 출력 : "+obj.toString());
         MemberEntity memberEntity = new MemberEntity();
         Map<String, Object> param = (Map<String, Object>) obj;
-        Integer empty = null;
+        String checkValue = null; //if문 내에서 값 오류 발생시 점검용 변수
 
         try {
             if (param.isEmpty()) {
@@ -28,56 +28,71 @@ public class Member {
                 return memberEntity;
             } else {
                 if (param.containsKey("SEQ")) {
+                    checkValue = "SEQ";
                     memberEntity.setSEQ((int) param.get("SEQ"));
                 }
                 if (param.containsKey("id")) {
+                    checkValue = "id";
                     memberEntity.setID((String) param.get("id"));
                 }
                 if (param.containsKey("password")) {
+                    checkValue = "password";
                     memberEntity.setPASSWORD((String) param.get("password"));
+                    
                 }
                 if (param.containsKey("name")) {
+                    checkValue = "name";
                     memberEntity.setNAME((String) param.get("name"));
                 }
                 if (param.containsKey("email")) {
+                    checkValue = "email";
                     memberEntity.setEMAIL((String) param.get("email"));
                 }
                 if (param.containsKey("phone")) {
+                    checkValue = "phone";
                     memberEntity.setPHONE((String) param.get("phone"));
                 }
                 if (param.containsKey("zipcode")) {
-                    if (param.get("zipcode").equals("")) {
-                        memberEntity.setZIPCODE(empty);
+                    if ("".equals(param.get("zipcode"))) {
+                        checkValue = "zipcode";
+                        memberEntity.setZIPCODE(null);
                     } else if (param.get("zipcode") instanceof String) {
+                        checkValue = "zipcode";
                         memberEntity.setZIPCODE(Integer.parseInt(String.valueOf(param.get("zipcode"))));
                     } else {
+                        checkValue = "zipcode";
                         memberEntity.setZIPCODE((int) param.get("zipcode"));
                     }
                 }
                 if (param.containsKey("roadaddr")) {
+                    checkValue = "roadaddr";
                     memberEntity.setROADADDR((String) param.get("roadaddr"));
                 }
                 if (param.containsKey("detailaddr")) {
+                    checkValue = "detailaddr";
                     memberEntity.setDETAILADDR((String) param.get("detailaddr"));
                 }
                 if (param.containsKey("create_date")) {
+                    checkValue = "create_date";
                     memberEntity.setCREATE_DATE((Timestamp) param.get("create_date"));
                 }
                 if (param.containsKey("update_date")) {
+                    checkValue = "update_date";
                     memberEntity.setUPDATE_DATE((Timestamp) param.get("update_date"));
                 }
                 if (param.containsKey("operator")) {
+                    checkValue = "operator";
                     memberEntity.setSEQ((int) param.get("operator"));
                 }
                 log.debug("objToEntity에서 memberEntity 출력 : "+memberEntity.toString());
                 log.debug("objToEntity에서 Map 에 들어온 값 처리 완료");
             }
         } catch (NullPointerException e) {
-            log.error("objToEntity에서 memberEntity 오류 발생 NullPointerException");
-            return memberClear(memberEntity);
+            log.error("objToEntity에서" + checkValue + " memberEntity 오류 발생 NullPointerException");
+            throw new RuntimeException(e);
         } catch (Exception e){
-            log.error("objToEntity에서 memberEntity 오류 발생 Exception");
-            return memberClear(memberEntity);
+            log.error("objToEntity에서" + checkValue + " memberEntity 오류 발생 Exception");
+            throw new RuntimeException(e);
         }
 
         return memberEntity;
