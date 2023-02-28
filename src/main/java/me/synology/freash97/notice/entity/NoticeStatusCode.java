6c68fcc7474abcdcc25f033095687a0a983ddb7f
YujinHwang-co <bineex2@naver.com>
2023-02-28 22:23:37 +0900

    게시글 로직 수정_상태코드 enum 제외

--- src/main/java/me/synology/freash97/notice/entity/NoticeStatusCode.java
+++ src/main/java/me/synology/freash97/notice/entity/NoticeStatusCode.java
@@ -1,10 +1,11 @@
 package me.synology.freash97.notice.entity;
 
 /**
- * date: 2023.02.06
+ * date: 2023.02.28
  * name: NoticeStatusCode.java
- * desc: 게시글 상태코드 enum 선언
- */
+ * desc: 게시글 상태코드 enum 제외 -> 미사용
+
 public enum NoticeStatusCode {
     A, U, D     // ADD, UPDATE, DELETE
 }
+ */
