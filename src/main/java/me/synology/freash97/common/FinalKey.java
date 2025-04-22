fa56d3739356b51eefa993f188ef0fc730bd45c0
iptablecommandcode <iptablecommandcode@gmail.com>
2025-04-22 23:20:00 +0900

    프로젝트 gradle 변경을 위한 데이터 초기화

--- src/main/java/me/synology/freash97/common/FinalKey.java
+++
@@ -1,7 +0,0 @@
-package me.synology.freash97.common;
-
-public class FinalKey {
-    //성공 실패 여부 String
-    public final String STRINGSUCCESS = "success";
-    public final String STRINGFAIL = "fail";
-}
