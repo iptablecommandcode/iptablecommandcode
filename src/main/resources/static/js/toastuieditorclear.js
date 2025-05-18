bf5b27cc35b63cb2bee9d330b2274359117da63e
iptablecommandcode <iptablecommandcode@gmail.com>
2025-05-18 22:29:27 +0900

    게시글 생성, 삭제, 수정 처리 완료

--- src/main/resources/static/js/toastuieditorclear.js
+++ src/main/resources/static/js/toastuieditorclear.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // 기존 내용이 textarea에 있을 경우, 초기값으로 사용
+    const initialContent = document.getElementById('hiddenContent').textContent || '';
+
     const editor = new toastui.Editor({
         el: document.querySelector('#editor'),
         height: '500px',
         initialEditType: 'wysiwyg',
         previewStyle: 'vertical',
+        initialValue: initialContent,
         hooks: {
             addImageBlobHook: async (blob, callback) => {
                 const formData = new FormData();
