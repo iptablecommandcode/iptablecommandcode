e178fb433d06e7a4dccb9e8ca52a4038584bbf34
박치원 <iptablecommandcode@gmail.com>
2026-07-01 15:59:31 +0900

    enum 상수 선언 변경

--- src/main/java/me/synology/freash97/common/enumconfig/ResultValue.java
+++ src/main/java/me/synology/freash97/common/enumconfig/ResultValue.java
@@ -1,8 +1,8 @@
 package me.synology.freash97.common.enumconfig;
 
 public enum ResultValue {
-    succ("success"),
-    fail("fail");
+    SUCC("success"),
+    FAIL("fail");
 
     private final String value;
 
